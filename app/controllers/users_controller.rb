80363719a78e6d23e2b4ac41cb4b620b577cde90
cesarfuster <cfuster@uol.com.br>
2018-06-18 07:25:12 -0300

    Coffee script to animate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,22 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  # GET /users
-  # GET /users.json
   def index
     @users = User.all
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
   end
 
-  # GET /users/new
   def new
     @user = User.new
+    # COMENTÁRIO: O build inicializa a página com 1 nested já renderizado
+    @experience = @user.experiences.build
   end
 
-  # GET /users/1/edit
   def edit
   end
 
-  # POST /users
-  # POST /users.json
   def create
     @user = User.new(user_params)
 
@@ -37,8 +31,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
       if @user.update(user_params)
@@ -51,8 +43,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     @user.destroy
     respond_to do |format|
@@ -62,13 +52,12 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name)
+      # COMENTÁRIO: Aqui estamos permitindo a passagem dos parâmetros do nested attribute "experiences"
+      params.require(:user).permit(:name, experiences_attributes: [:id, :company, :period, :occupation, :_destroy])
     end
 end
